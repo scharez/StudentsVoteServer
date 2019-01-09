f3526d55d0f4dd5da4d62027b224e72cce74b6b1
MarkusWagenleitner <markus.wagenleitner.htl@gmail.com>
2019-01-09 11:58:28 +0100

    loginCheck
    hallo max

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -1,4 +1,17 @@
 package repository;
 
 public class Repository {
+
+    public String loginCheck(String username, String password) {
+        if(username.equals("")) {
+            if(password.equals("")) {
+                return "login successful";
+            } else {
+                return "invalid password";
+            }
+        } else {
+            return "invalid username";
+        }
+    }
+
 }
--- src/main/java/service/StudentsVoteService.java
+++ src/main/java/service/StudentsVoteService.java
@@ -1,5 +1,7 @@
 package service;
 
+import repository.Repository;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,10 +10,19 @@ import javax.ws.rs.core.MediaType;
 @Path("persons")
 public class StudentsVoteService {
 
+    private Repository repo = new Repository();
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("message")
     public String message() {
         return " REST Service powered by scharez.at ";
     }
+
+    @GET
+    @Path("loginCheck")
+    public String loginCheck(String username, String password) {
+        return repo.loginCheck(username, password);
+    }
+
 }
