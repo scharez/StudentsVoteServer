c444a64e66c27605cfa9f2d07890a85426d6b210
MarkusWagenleitner <markus.wagenleitner.htl@gmail.com>
2019-01-10 12:19:01 +0100

    loginCheck
    hallo max

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -2,19 +2,11 @@ package entity;
 
 public class User {
 
-     String password;
-     String username;
+    String username;
+    String password;
 
     public User(String username, String password) {
         this.username = username;
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
@@ -25,5 +17,13 @@ public class User {
     public void setUsername(String username) {
         this.username = username;
     }
-}
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
