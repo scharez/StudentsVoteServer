673056b4ca0a23c3e939ba4b05711b6f7b1c8300
Maqs <maximilianwiesmayr@gmx.at>
2019-01-16 10:58:19 +0100

    Service: register added Repository: Repository mit Singelton aufgesetzt (möglicherweise änderung nötig) Repository Funktionsansatzt erstellt.

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -1,8 +1,24 @@
 package repository;
 
+import entity.ReturningOfficer;
 import entity.User;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Repository {
+
+    private List<ReturningOfficer> rOfficer = new LinkedList<>();
+
+    private static Repository instance;
+    private Repository() {
+    }
+    public static Repository getInstance() {
+        if (instance == null) {
+            instance = new Repository();
+        }
+        return instance;
+    }
 
     public String loginCheck(User user) {
 
@@ -12,6 +28,12 @@ public class Repository {
         return "TEST";
     }
 
+    public String register() {
+
+
+
+        return "You are now Registered";
+    }
 }
 
 /*
--- src/main/java/service/StudentsVoteService.java
+++ src/main/java/service/StudentsVoteService.java
@@ -1,17 +1,19 @@
 package service;
 
+import entity.ReturningOfficer;
 import repository.Repository;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import entity.User;
 
 @Path("sv")
 public class StudentsVoteService {
 
-    private Repository repo = new Repository();
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -24,7 +26,16 @@ public class StudentsVoteService {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("login")
     public String loginCheck(User user) {
-        return repo.loginCheck(user);
+        return Repository.getInstance().loginCheck(user);
     }
+
+    @Path("register")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String register(ReturningOfficer rs){
+        return Repository.getInstance().register();
+    }
+
+
 
 }
